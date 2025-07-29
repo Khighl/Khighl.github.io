10d2dffbd5c03880757b0abcc73eacaa76ad4a5b
Khighl <18510805232@163.com>
2025-07-29 19:25:04 +0800

    Site updated: 2025-07-29 19:24:50

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1753758912822";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1753788204009";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
